129f6f11ab66592bd51ab1c1916f1cafb3870f39
James Hughes <james@pyrosoftsolutions.co.uk>
2023-01-22 19:35:45 +0000

    Added bar intervals

--- src/PgfPlotsSdk.Tests/SyntaxTree/Nodes/Plots/PlotNodeTests.cs
+++ src/PgfPlotsSdk.Tests/SyntaxTree/Nodes/Plots/PlotNodeTests.cs
@@ -31,7 +31,7 @@ public class PlotNodeTests
 			Smooth = false
 		};
 
-		string expected = """
+		const string expected = """
 						\addplot[color=cyan, mark=pentagon, mark size=0.5, line width=0.2, only marks]
 						plot coordinates {(0,1) (2,3) (4,5)};
 
@@ -60,7 +60,7 @@ public class PlotNodeTests
 			FillOpacity = 0.2f
 		};
 
-		string expected = """
+		const string expected = """
 						\addplot[fill opacity=0.2, dashed, fill=magenta]
 						plot coordinates {(0,1) (2,3) (4,5)};
 
@@ -87,11 +87,42 @@ public class PlotNodeTests
 			Colour = LatexColour.Green,
 			Fill = LatexColour.Red,
 			FillOpacity = 0.4f,
-			BarType = BarType.XBar
+			BarType = BarType.XBar,
 		};
 		
-		string expected = """
+		const string expected = """
 						\addplot[color=green, fill opacity=0.4, xbar, fill=red]
+						plot coordinates {(0,1) (2,3) (4,5)};
+
+						""";
+		
+		OptionsCollectionNode optionsCollectionNode = new(options.GetOptionsDictionary());
+		RawDataCollectionNode rawDataCollectionNode = new(Data);
+		
+		PlotNode plotNode = new();
+		plotNode.AddChild(optionsCollectionNode);
+		plotNode.AddChild(rawDataCollectionNode);
+
+		PgfPlotSyntaxTree tree = new(plotNode);
+		string res = tree.GenerateSource();
+		
+		res.ShouldBe(expected);
+	}
+	
+	[Fact]
+	public void CanGenerateXBarIntervalChart()
+	{
+		PlotOptions options = new()
+		{
+			Colour = LatexColour.Green,
+			Fill = LatexColour.Red,
+			FillOpacity = 0.4f,
+			BarType = BarType.XBarInterval,
+			BarWidth = 0.1f
+		};
+		
+		const string expected = """
+						\addplot[color=green, fill opacity=0.4, xbar interval, bar width=0.1, fill=red]
 						plot coordinates {(0,1) (2,3) (4,5)};
 
 						""";
@@ -120,8 +151,39 @@ public class PlotNodeTests
 			BarType = BarType.YBar
 		};
 		
-		string expected = """
+		const string expected = """
 						\addplot[color=green, fill opacity=0.4, ybar, fill=red]
+						plot coordinates {(0,1) (2,3) (4,5)};
+
+						""";
+		
+		OptionsCollectionNode optionsCollectionNode = new(options.GetOptionsDictionary());
+		RawDataCollectionNode rawDataCollectionNode = new(Data);
+		
+		PlotNode plotNode = new();
+		plotNode.AddChild(optionsCollectionNode);
+		plotNode.AddChild(rawDataCollectionNode);
+
+		PgfPlotSyntaxTree tree = new(plotNode);
+		string res = tree.GenerateSource();
+		
+		res.ShouldBe(expected);
+	}
+	
+	[Fact]
+	public void CanGenerateYBarIntervalChart()
+	{
+		PlotOptions options = new()
+		{
+			Colour = LatexColour.Green,
+			Fill = LatexColour.Red,
+			FillOpacity = 0.4f,
+			BarType = BarType.YBarInterval,
+			BarWidth = 0.2f
+		};
+		
+		const string expected = """
+						\addplot[color=green, fill opacity=0.4, ybar interval, bar width=0.2, fill=red]
 						plot coordinates {(0,1) (2,3) (4,5)};
 
 						""";
