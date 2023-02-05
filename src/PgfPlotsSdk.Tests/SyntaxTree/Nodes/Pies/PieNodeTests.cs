ce7daf08ee924f6862c999512ea0d21ff10863ef
James Hughes <james@pyrosoftsolutions.co.uk>
2023-02-05 20:26:26 +0000

    test: Fleshed out pie node tests

--- src/PgfPlotsSdk.Tests/SyntaxTree/Nodes/Pies/PieNodeTests.cs
+++ src/PgfPlotsSdk.Tests/SyntaxTree/Nodes/Pies/PieNodeTests.cs
@@ -2,6 +2,7 @@
 using PgfPlotsSdk.Internal.SyntaxTree.Nodes.Options;
 using PgfPlotsSdk.Internal.SyntaxTree.Nodes.Pies;
 using PgfPlotsSdk.Internal.SyntaxTree.Nodes.Pies.Data;
+using PgfPlotsSdk.Public.ElementDefinitions.Enums;
 using PgfPlotsSdk.Public.ElementDefinitions.Options;
 using PgfPlotsSdk.Public.ElementDefinitions.Plots.Data;
 using Shouldly;
@@ -10,7 +11,10 @@ namespace PgfPlotsSdk.Tests.SyntaxTree.Nodes.Pies;
 
 public class PieNodeTests
 {
-	
+	private static readonly PieChartSliceData<int> ChartSliceOne = new(5);
+	private static readonly PieChartSliceData<int> ChartSliceTwo = new(10);
+	private static readonly PieChartSliceData<int> ChartSliceThree = new(15);
+	private static readonly RawPieSliceCollectionNode<int> Data = new(ChartSliceOne, ChartSliceTwo, ChartSliceThree);
 	
 	[Fact]
 	public void CanGenerateSimplePieChart()
@@ -18,24 +22,151 @@ public class PieNodeTests
 		PieChartOptions options = new()
 		{
 			CentrePosition = new(1, 1),
-			ReferenceSum = 30
+			ReferenceSum = 30,
+			Rotation = 90,
+			BeforeNumberText = "\\$"
 		};
 
-		PieChartSliceData<int> chartSliceOne = new(5);
-		PieChartSliceData<int> chartSliceTwo = new(10);
-		PieChartSliceData<int> chartSliceThree = new(15);
-
 		const string expected = """
-								\pie[pos={1,1}, sum=30]
+								\pie [pos={1,1}, rotate=90, sum=30, before number=\$]
 								{5, 10, 15};
 								
 								""";
 		
 		OptionsCollectionNode optionsCollectionNode = new(options.GetOptionsDictionary());
-		RawPieSliceCollectionNode<int> data = new(new[] { chartSliceOne, chartSliceTwo, chartSliceThree });
+		
 
 		PieChartNode pieNode = new();
 		pieNode.AddChild(optionsCollectionNode);
+		pieNode.AddChild(Data);
+
+		PgfPlotSyntaxTree tree = new(pieNode);
+		string res = tree.GenerateSource();
+		
+		res.ShouldBe(expected);
+	}
+
+	[Fact]
+	public void CanGeneratePolarPieChart()
+	{
+		PieChartOptions options = new()
+		{
+			PieChartType = PieType.Polar
+		};
+		
+		const string expected = """
+								\pie [polar]
+								{5, 10, 15};
+								
+								""";
+		
+		OptionsCollectionNode optionsCollectionNode = new(options.GetOptionsDictionary());
+		
+
+		PieChartNode pieNode = new();
+		pieNode.AddChild(optionsCollectionNode);
+		pieNode.AddChild(Data);
+
+		PgfPlotSyntaxTree tree = new(pieNode);
+		string res = tree.GenerateSource();
+		
+		res.ShouldBe(expected);
+	}
+	
+	[Fact]
+	public void CanGenerateCloudPieChart()
+	{
+		PieChartOptions options = new()
+		{
+			PieChartType = PieType.Cloud
+		};
+		
+		const string expected = """
+								\pie [cloud]
+								{5, 10, 15};
+								
+								""";
+		
+		OptionsCollectionNode optionsCollectionNode = new(options.GetOptionsDictionary());
+		
+
+		PieChartNode pieNode = new();
+		pieNode.AddChild(optionsCollectionNode);
+		pieNode.AddChild(Data);
+
+		PgfPlotSyntaxTree tree = new(pieNode);
+		string res = tree.GenerateSource();
+		
+		res.ShouldBe(expected);
+	}
+	
+	[Fact]
+	public void CanGenerateSquarePieChart()
+	{
+		PieChartOptions options = new()
+		{
+			PieChartType = PieType.Square
+		};
+		
+		const string expected = """
+								\pie [square]
+								{5, 10, 15};
+								
+								""";
+		
+		OptionsCollectionNode optionsCollectionNode = new(options.GetOptionsDictionary());
+		
+
+		PieChartNode pieNode = new();
+		pieNode.AddChild(optionsCollectionNode);
+		pieNode.AddChild(Data);
+
+		PgfPlotSyntaxTree tree = new(pieNode);
+		string res = tree.GenerateSource();
+		
+		res.ShouldBe(expected);
+	}
+	
+	[Fact]
+	public void CanGenerateExplodedPieChart()
+	{
+		PieChartOptions options = new()
+		{
+			SliceExplosionFactors = new(){0.1f, 0.2f, 0.3f}
+		};
+		
+		const string expected = """
+								\pie [explode={0.1,0.2,0.3}]
+								{5, 10, 15};
+								
+								""";
+		
+		OptionsCollectionNode optionsCollectionNode = new(options.GetOptionsDictionary());
+		
+
+		PieChartNode pieNode = new();
+		pieNode.AddChild(optionsCollectionNode);
+		pieNode.AddChild(Data);
+
+		PgfPlotSyntaxTree tree = new(pieNode);
+		string res = tree.GenerateSource();
+		
+		res.ShouldBe(expected);
+	}
+	
+	[Fact]
+	public void CanGeneratePieChartWithLabels()
+	{
+		PieChartSliceData<int> sliceOne = new(52, "People who believe anything they're told");
+		PieChartSliceData<int> sliceTwo = new(48, "People who understand basic economics");
+		RawPieSliceCollectionNode<int> data = new(sliceOne, sliceTwo);
+		
+		const string expected = """
+								\pie {52/People who believe anything they're told, 48/People who understand basic economics};
+								
+								""";
+		
+		PieChartNode pieNode = new();
 		pieNode.AddChild(data);
 
 		PgfPlotSyntaxTree tree = new(pieNode);
